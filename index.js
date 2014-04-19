08cc32738bd289950e3a177a58b8bce9fbf2d2ac
chetandhembre <chetandhembre@gmail.com>
2014-04-20 00:46:57 +0530

    convert this project into simple node module

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
  */
 
 var lrequire =  require('local-require');
-lrequire.register('demo', {
+lrequire.registerSync('demo', {
     '1.js' : './test/1.js',
     'test1' : './test1'
-}, function(err) {
-    console.log(err);
-    require('demo/1.js');
-    console.log(require('demo/test1').test1());
 });
 
+
+exports.test = function() {
+    require('demo/1.js');
+};
+
+exports.test1 = function() {
+    require('demo/test1').test1();
+};
